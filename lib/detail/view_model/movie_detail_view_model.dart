a022a670781bc8f91df63c0734977684e93c0db6
claudialukita <claudialukitadewi@gmail.com>
2021-08-19 19:54:39 +0700

    assignment day 4

--- lib/detail/view_model/movie_detail_view_model.dart
+++ lib/detail/view_model/movie_detail_view_model.dart
@@ -1,18 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:moviedb/core/models/async_state.dart';
 import 'package:moviedb/core/models/movie_detail.dart';
-import 'package:moviedb/core/providers/storage_provider.dart';
 import 'package:moviedb/core/services/movie_service.dart';
 
 final movieDetailViewModelProvider =
     StateNotifierProvider<MovieDetailViewModel, AsyncState<MovieDetail>>(
-        (ref) => MovieDetailViewModel(ref.read(movieServiceProvider), ref.read(storageProvider)));
+        (ref) => MovieDetailViewModel(ref.read(movieServiceProvider)));
 
 class MovieDetailViewModel extends StateNotifier<AsyncState<MovieDetail>> {
   final MovieService _movieService;
-  final FlutterSecureStorage _secureStorage;
-  MovieDetailViewModel(this._movieService, this._secureStorage) : super(Initial(null));
+  MovieDetailViewModel(this._movieService) : super(Initial(null));
 
   loadData(int movieId) async {
     state = Loading(state.data);
